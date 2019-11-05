868fde9729abaa8d7286492d7ee74594c11a2b64
Luchanskiy Oleg <luchansodev@gmail.com>
2019-11-05 18:44:14 +0300

    feat: add apollo boost

--- packages/client/src/components/App/App.tsx
+++ packages/client/src/components/App/App.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { client } from "../../apollo";
 import { Heatmap } from "../Heatmap/Heatmap";
 import { AddPage } from "../AddPage/AddPage";
 
 export const Router = () => {
   return (
-    <BrowserRouter>
-      <React.Fragment>
-        <Route exact path="/">
-          <Heatmap />
-        </Route>
-        <Route path="/add">
-          <AddPage/>
-        </Route>
-      </React.Fragment>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <React.Fragment>
+          <Route exact path="/">
+            <Heatmap />
+          </Route>
+          <Route path="/add">
+            <AddPage />
+          </Route>
+        </React.Fragment>
+      </BrowserRouter>
+    </ApolloProvider>
   );
 };
--- packages/client/src/components/Table/Table.tsx
+++ packages/client/src/components/Table/Table.tsx
@@ -1,52 +1,75 @@
-import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import Paper from "@material-ui/core/Paper";
 import MaterialTable from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
+import { loader } from "graphql.macro";
+import React from "react";
 import styled from "styled-components";
-import { tempData } from "./TempData";
-import {
-  toTableBody,
-  toTableHead
-} from "../../utils/project-parser/projects-parser";
+import { toTableBody, toTableHead } from "../../utils/project-parser/projects-parser";
 import { AddSourceButton } from "../AddSourceButton/AddSourceButton";
+import { tempData } from "./TempData";
 
 const RootLayout = styled.div`
   margin: 64px;
 `;
 
 const example = [toTableHead(tempData), toTableBody(tempData)] as const;
+const query = loader('./getDependenciesMapFromGit.gql');
 
-export const Table = () => (
-  <RootLayout>
-    <Paper>
-      <MaterialTable size="small">
-        <TableHead>
-          <TableRow>
-            <TableCell>
-              <AddSourceButton onClick={() => {}} />
-            </TableCell>
-            {example[0].map(project => (
-              <TableCell align="right" key={project}>
-                {project}
+export const Table = () => {
+  const { loading, error, data } = useQuery(query);
+  console.log(data);
+
+  if (loading) {
+    return <React.Fragment>loading</React.Fragment>;
+  }
+  if (error) {
+    return <React.Fragment>{error.message.toString()}</React.Fragment>
+  }
+  if (data) {
+    return (
+      <Paper style={{ padding: '8px 16px', marginBottom: 24 }}>
+        <pre>data: { data && JSON.stringify(data, null, 4) }</pre>
+      </Paper>
+    )
+  }
+
+  return (
+    <RootLayout>
+      <Paper>
+        <MaterialTable size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                <AddSourceButton onClick={() => {}} />
               </TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {example[1].map(row => (
-            <TableRow key={row[0]} hover>
-              <TableCell>{row[0]}</TableCell>
-              {row.map(
-                (ver, index) =>
-                  index !== 0 && <TableCell align="right" key={index + ver}>{ver}</TableCell>
-              )}
+              {example[0].map(project => (
+                <TableCell align="right" key={project}>
+                  {project}
+                </TableCell>
+              ))}
             </TableRow>
-          ))}
-        </TableBody>
-      </MaterialTable>
-    </Paper>
-  </RootLayout>
-);
+          </TableHead>
+          <TableBody>
+            {example[1].map(row => (
+              <TableRow key={row[0]} hover>
+                <TableCell>{row[0]}</TableCell>
+                {row.map(
+                  (ver, index) =>
+                    index !== 0 && (
+                      <TableCell align="right" key={index + ver}>
+                        {ver}
+                      </TableCell>
+                    )
+                )}
+              </TableRow>
+            ))}
+          </TableBody>
+        </MaterialTable>
+      </Paper>
+    </RootLayout>
+  );
+};
