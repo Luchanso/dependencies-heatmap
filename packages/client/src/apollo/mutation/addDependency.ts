0f5532db250e1bb0db09d8cae6565c25c0a73aeb
Luchanskiy Oleg <luchansodev@gmail.com>
2020-01-14 17:32:32 +0300

    chore: temp work

--- packages/client/src/apollo/mutation/addDependency.ts
+++ packages/client/src/apollo/mutation/addDependency.ts
@@ -1,5 +1,15 @@
 import { BaseResolver } from "../resolvers";
 
-export const addDependency: BaseResolver<{ url: string }> = (root, { url }) => {
+const DEPENDENCY_KEY = 'dependency';
 
+export const getDependencies = () =>
+  JSON.parse(localStorage.getItem(DEPENDENCY_KEY) || '{}');
+
+export const setDependencies = (dependencies: any) =>
+  localStorage.setItem(DEPENDENCY_KEY, dependencies);
+
+export const addDependency: BaseResolver<{ url: string }> = (root, { url }) => {
+  // TODO: написать функцию которая кладет в стор по определенному адресу данные и достаёт их
+  // localStorage
+  const dependencies = getDependencies();
 }
