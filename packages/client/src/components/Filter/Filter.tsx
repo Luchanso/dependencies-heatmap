85f058cc6a83e2e811047d4786a2508f272f6ad0
Oleg Loutchansky <luchansodev@gmail.com>
2020-04-17 02:04:58 +0300

    fix: some fixes

--- packages/client/src/components/Filter/Filter.tsx
+++ packages/client/src/components/Filter/Filter.tsx
@@ -1,39 +1,41 @@
-import { Chip, TextField } from "@material-ui/core";
-import { Autocomplete } from "@material-ui/lab";
-import React, { ChangeEvent } from "react";
-import { useFilters } from "./useFilters";
+// import { Chip, TextField } from "@material-ui/core";
+// import { Autocomplete } from "@material-ui/lab";
+// import React, { ChangeEvent } from "react";
+// import { useFilters } from "./useFilters";
 
 export const Filter = () => {
-  const { availableFilters, setFilters, filters } = useFilters();
 
-  function handleChange(event: ChangeEvent<{}>, value: string[]) {
-    setFilters(value);
-  }
+  return null;
+  // const { availableFilters, setFilters, filters } = useFilters();
 
-  return (
-    <Autocomplete
-      autoComplete
-      id="filter"
-      multiple
-      options={availableFilters || []}
-      filterSelectedOptions
-      getOptionLabel={option => option}
-      onChange={handleChange}
-      value={filters}
-      renderTags={(value: any[], getTagProps) =>
-        value.map((option, index) => (
-          <Chip label={option} color="primary" {...getTagProps({ index })} />
-        ))
-      }
-      renderInput={params => (
-        <TextField
-          {...params}
-          placeholder={availableFilters?.slice(0, 3).join(', ')}
-          variant="filled"
-          label="filter"
-          fullWidth
-        />
-      )}
-    />
-  );
+  // function handleChange(event: ChangeEvent<{}>, value: string[]) {
+  //   setFilters(value);
+  // }
+
+  // return (
+  //   <Autocomplete
+  //     autoComplete
+  //     id="filter"
+  //     multiple
+  //     options={availableFilters || []}
+  //     filterSelectedOptions
+  //     getOptionLabel={option => option}
+  //     onChange={handleChange}
+  //     value={filters}
+  //     renderTags={(value: any[], getTagProps) =>
+  //       value.map((option, index) => (
+  //         <Chip label={option} color="primary" {...getTagProps({ index })} />
+  //       ))
+  //     }
+  //     renderInput={params => (
+  //       <TextField
+  //         {...params}
+  //         placeholder={availableFilters?.slice(0, 3).join(', ')}
+  //         variant="filled"
+  //         label="filter"
+  //         fullWidth
+  //       />
+  //     )}
+  //   />
+  // );
 };
