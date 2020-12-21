5f6172dab530acb83c67f0064864ad09c5310cb4
Arver1 <aactepanov@mail.ru>
2020-12-21 20:34:47 +0300

    feat(*): added list projects from url

--- packages/client/src/apollo/query/projects.ts
+++ packages/client/src/apollo/query/projects.ts
@@ -1,8 +1,11 @@
+import qs from 'qs';
 import { BaseResolver } from "../resolvers";
 import { STORE_KEYS, getItem } from "../../localStorage";
 
 export const projects: BaseResolver = () => {
-  const projects = getItem<string[]>(STORE_KEYS.projects) || [];
+  const projects = window.location.search
+      ? qs.parse(window.location.search)
+      : getItem<string[]>(STORE_KEYS.projects) || [];
 
   return projects;
 };
